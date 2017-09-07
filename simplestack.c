7d583124acc0a7f12f768a96146f6a18a7f65727
MD XF <rkcoder2002@gmail.com>
2017-09-06 19:34:23 -0700

    Update simplestack.c

--- simplestack.c
+++ simplestack.c
@@ -32,6 +32,9 @@ int execute(wint_t*,int);
 void do_skip(void);
 int do_jump(void);
 int implicit_input(int,int);
+#ifndef _WIN32
+int itoa(int,char*,int);
+#endif
 
 struct _stack {
   int val;
@@ -893,3 +896,60 @@ int wcstoi(wint_t *str)
 
     return retval;
 }
+
+#ifndef _WIN32
+/* A utility function to reverse a string, credit to http://www.geeksforgeeks.org/implement-itoa/  */
+void reverse(char str[], int length)
+{
+    int start = 0;
+    int end = length -1;
+    while (start < end)
+    {
+        swap(*(str+start), *(str+end));
+        start++;
+        end--;
+    }
+}
+ 
+// Implementation of itoa()
+char* itoa(int num, char* str, int base)
+{
+    int i = 0;
+    bool isNegative = false;
+ 
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+ 
+    // In standard itoa(), negative numbers are handled only with 
+    // base 10. Otherwise numbers are considered unsigned.
+    if (num < 0 && base == 10)
+    {
+        isNegative = true;
+        num = -num;
+    }
+ 
+    // Process individual digits
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num/base;
+    }
+ 
+    // If number is negative, append '-'
+    if (isNegative)
+        str[i++] = '-';
+ 
+    str[i] = '\0'; // Append string terminator
+ 
+    // Reverse the string
+    reverse(str, i);
+ 
+    return str;
+}
+#endif
